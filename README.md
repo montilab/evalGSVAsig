f357f293d06a8a847a3714159e9bc9fcdef419b1
Lina Kroehling <lkroeh@bu.edu>
2023-06-09 10:28:39 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# evalGSVAsig
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of evalGSVAsig is to ...
+
+## Installation
+
+You can install the development version of evalGSVAsig like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(evalGSVAsig)
+## basic example code
+```
+
---
+++ tests/testthat/test-numeric_summary.R
@@ -0,0 +1,16 @@
+test_that("numeric_summary function works", {
+
+  x <- c(1,3,5,7,9)
+  result <- numeric_summary(x)
+
+  expected_result <- c(min=1.0, max=9.0,
+                        mean=5.0, sd=3.1620,
+                        length = 5, Nmiss=0)
+  expect_equal( round(result,3), expected_result)
+})
+
+test_that("numeric_summary function works", {
+  x <- c(F,F,T,T,T,F)
+  expect_error( numeric_summary(x) )
+
+})
